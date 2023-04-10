d756b22492f8c296e15a6a7fecc3c3626b3744b4
jdhalleine-test <jdhalleine1@gmail.com>
2023-04-10 16:48:17 +0200

    add nat-gateway

--- main.tf
+++ main.tf
@@ -22,3 +22,20 @@ module "vpc" {
 
 }
 
+#create nat-gateways
+module "nat-gateway" {
+
+  source = "git@github.com:jdhalleine/aws-terraform.git//nat-gateway"
+
+  project_name               = local.project_name
+  environment                = local.environment
+  public_subnet_az1_id       = module.vpc.public_subnet_az1_id
+  public_subnet_az2_id       = module.vpc.public_subnet_az2_id
+  internet_gateway           = module.vpc.internet_gateway
+  vpc_id                     = module.vpc.vpc_id
+  private_app_subnet_az1_id  = module.vpc.private_app_subnet_az1_id
+  private_data_subnet_az1_id = module.vpc.private_data_subnet_az1_id
+  private_app_subnet_az2_id  = module.vpc.private_app_subnet_az2_id
+  private_data_subnet_az2_id = module.vpc.private_app_subnet_az2_id
+
+}
